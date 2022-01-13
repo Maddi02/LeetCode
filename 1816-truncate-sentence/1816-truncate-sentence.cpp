69fbdbef7748e621ccbf8ef2b148ab9ced9270f4
Maddi <90851855+Maddi02@users.noreply.github.com>
2022-01-13 20:06:56 +0100

    Time: 2 ms (36.32%), Space: 6.2 MB (97.04%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.cpp
+++ 1816-truncate-sentence/1816-truncate-sentence.cpp
@@ -1,25 +1,9 @@
 class Solution {
 public:
     string truncateSentence(string s, int k) {
-       std::vector<std::string> a;
-        std::string save = "";
-        for (int i = 0; i < s.length(); i++) {
-            if (s.at(i) != ' ') {
-                save += s.at(i);
-            }
-            if (s.at(i) == ' ' || i == (s.length() -1) ) {
-                a.push_back(save);
-                save = "";
-            }
-        }
-        for(int i = 0 ; i < k ; i++)
-        {
-            if(i < k -1 )
-            save += a.at(i) + " ";
-            else{
-                save += a.at(i);
-            }
-        }
-        return save; 
+          for (int i = 0; i < s.size(); ++i)
+            if (s[i] == ' ' && --k == 0)
+                return s.substr(0, i);
+        return s;
     }
 };
