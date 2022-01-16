ae5b06352ea7744f96d4cf5ee0a9a208b85856d2
Maddi <90851855+Maddi02@users.noreply.github.com>
2022-01-16 11:21:22 +0100

    Time: 16 ms (48.56%), Space: 17.1 MB (63.67%) - LeetHub

---
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+            int longest = 0;
+            int beginning = 0;
+            int i = 0;
+            while (seats[i]==0)
+            {
+                beginning++;
+                i++;
+            }
+            int count = beginning;
+            for (; i<seats.size(); i++)
+            {
+                if (seats[i] == 0)
+                    count++;
+                else
+                {
+                    longest = std::max({count, longest});
+                    count = 0;
+                }
+            }
+            longest = longest%2==0?longest/2:longest/2+1;
+            return std::max({longest, count, beginning});
+        }
+};
